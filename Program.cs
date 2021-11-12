e5b29b823c9ec3a86c8d30f1d1082a1b28861bfb
Sasha <sashaparfiak007@gmail.com>
2021-11-12 19:44:01 +0300

    Open Pricing Calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -9,7 +10,16 @@ namespace EpamTest
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://code.visualstudio.com/docs/languages/csharp";
+            driver.Url = "https://cloud.google.com/";
+            IWebElement click1 = driver.FindElement(By.Name("q")); 
+            click1.Click();
+            click1.Clear();
+            click1.SendKeys("Google Cloud Platform Pricing Calculator");
+            click1.SendKeys(Keys.Enter);
+            Thread.Sleep(5000);
+            driver.FindElement(By.PartialLinkText("Google Cloud Platform Pricing Calculator")).Click();
+            Thread.Sleep(5000);
+            driver.Quit();
         }
     }
 }
